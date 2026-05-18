34003eb7bda76f349921043245df800a5ed45e35
Sufi_0181 <mohdsufiyan0181@gmail.com>
2026-05-18 06:30:07 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all JS code as newer version of code
+
+// alert(3+3) // we are usinng nodejs, not browser
+
+console.log (3+3)
+
+let name = "sufiyan"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to pwr 53
+// bigint => 
+
+// string => ""
+// boolean => true/false
+
+// null => standalone vlue
+// undefined =>
+// symbol => unique // to find uniqueness
+
+// object 
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);       // object
+
---
+++ 01_Basics/variables.js
@@ -0,0 +1,4 @@
+const accountId = 144553
+let accountEmail = "Sufi@gmail.com"
+var accountPass = "12345"
+accountCity = "Greater Noida"

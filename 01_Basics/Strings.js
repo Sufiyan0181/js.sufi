380ce9edc51a6702ec790059cb0dcd1a3c82ccb1
Sufi_0181 <mohdsufiyan0181@gmail.com>
2026-05-19 08:21:54 +0000

    Strings

---
+++ 01_Basics/Strings.js
@@ -0,0 +1,16 @@
+let a = "Sufi"
+
+let b = " Butcher"
+
+let c = a + b
+
+console.log(c)
+
+console.log(`hey my name is ${a} and my nickname is ${b}`)
+console.log(a[0])
+console.log(b[2])
+
+const s = "   Sufi   "
+
+console.log(s)
+console.log(s.trim())  // it will trim all extra spaces
